821e6132797fda792f8004405df5739a38d3f139
github username <github email address>
2017-08-04 10:23:17 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  state = {
+    username: "",
+    hometown: ""
+  }
+
+  handleUserChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+  handleHometownChange = (event) => {
+    this.setState({
+      hometown: event.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    this.props.store.dispatch({
+      type: 'ADD_USER', 
+      user: this.state
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.username} onChange={this.handleUserChange}/>
+          <input type="text" value={this.state.hometown} onChange={this.handleHometownChange}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,11 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = { users: []},
+  action){
+    switch(action.type) {
+
+      case 'ADD_USER':
+        let user = {username: action.user.username, hometown: action.user.hometown}
+        return {users: [...state.users, user]}
+      default:
+        return state;
+    }
 }
