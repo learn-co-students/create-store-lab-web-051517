e45b297f97978009725e0c49ff948fecd886219e
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-08-03 22:41:57 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,11 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state={users:[]}, action){
+  switch(action.type){
+    case 'ADD_USER':
+      state ={
+        users:[...state.users, action.user]
+      }
+      return state;
+    default:
+    return state;
+  }
 }
